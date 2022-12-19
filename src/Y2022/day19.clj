aa34c86071dd8171224a147ef7cf2e626db9108c
Benj <Benjamin.Schwerdtner@gmail.com>
2022-12-19 18:22:45 +0100

    Add notes

--- src/Y2022/day19.clj
+++ src/Y2022/day19.clj
@@ -1,77 +1,170 @@
 (ns Y2022.day19
   (:require [clojure.string :as str]))
 
-(def blueprint1
-  {:ore-robot {:ore 4}
-   :clay-robot {:ore 2}
-   :obsidian-robot {:ore 3 :clay 14}
-   :geoderobot {:ore 2 :obsidian 7}})
+(defn parse-blueprints [input]
+  (->>
+   (re-seq
+    #"Blueprint (\d+): Each ore robot costs (\d+) ore. Each clay robot costs (\d+) ore. Each obsidian robot costs (\d+) ore and (\d+) clay. Each geode robot costs (\d+) ore and (\d+) obsidian."
+    input)
+   (map rest)
+   (map #(map parse-long %))
+   (map
+    (fn
+      [[_
+        ore-cost
+        clay-ore
+        obsidian-ore
+        obsidian-clay
+        geode-ore
+        geode-obsidian]]
+      {:ore-robot {:ore ore-cost}
+       :clay-robot {:ore clay-ore}
+       :obsidian-robot {:ore obsidian-ore
+                        :clay obsidian-clay}
+       :geoderobot {:ore geode-ore
+                    :obsidian geode-obsidian}}))
+   (into [])))
 
-;; constructing takes 1 min
+(parse-blueprints (slurp "/home/benj/repos/clojure/aoc/inputs/2022/day19"))
 
-(def state
+
+(def blueprints
+  [{:ore-robot {:ore 4},
+    :clay-robot {:ore 2},
+    :obsidian-robot {:ore 3, :clay 14},
+    :geoderobot {:ore 2, :obsidian 7}}
+   {:ore-robot {:ore 2},
+    :clay-robot {:ore 3},
+    :obsidian-robot {:ore 3, :clay 8},
+    :geoderobot {:ore 3, :obsidian 12}}])
+
+(def start-state
   {:robots {:ore 1 :clay 0 :geode 0 :obsidian 0}
    :resources {:ore 0 :clay 0 :obsidian 0 :geode 0}
    :time 24})
 
-
-(defn step [{:keys [time robots resources] :as state}]
+;; if you build, your factory is locked until you step-time
+;; and your robot is functional after 1 time
+(defn step-time [{:keys [time robots resources building] :as state}]
   (-> state
       (update :time dec)
-      (update :resources (fn [m] (merge-with + m robots)))))
+      (update :resources (fn [m] (merge-with + m robots)))
+      (update-in [:robots building] inc)
+      (dissoc :building)))
+
+(defn blueprint-molecules [blueprints]
+  (map-indexed
+   (fn
+     [idx b]
+     (->
+      start-state
+      (assoc :blueprint b)
+      (assoc :blueprint-n idx)))
+   blueprints))
+
+;; chemical computing
+;; each blueprint would be
+;; a starting kind of molecule I think
+
+;; then, are there "decision" molekules?
+;; or should I just step all the molekules and sometimes they decide to split into 2
+
+(def molecules
+  ;; I model decisions as molecules
+  ;; then the facories/states are bumping into decisions
+  ;; if you bump into the descicion to build something
+  ;; but you don't have the resources, you wait instead
+  ;; time step is a second kind of molecule
+  [{:decision [:build :clay]}
+   {:decision [:build :ore]}
+   {:decision [:build :geode]}
+   {:decision [:build :obsidian]}
+   {:decision [:dont-build]}
+   {:time 1}
+
+   ]
+  )
+
+(def factory? :robots)
+
+(defn max-reaction-reducing [[a b]]
+  (if (> a b) [a] [a b]))
+
+
+(defn robot-factory-stepping [[a b]]
+  (if-let
+      [[factory move]
+       (cond
+         (and (factory? a)
+              (factory? b))
+         nil
+         (factory? a)
+         [a b]
+         (factory? b)
+         [b a]
+         :else nil)]
+    ;; apply the move
+      '()
+      (cond
+        (:time move)
+        ;; time doesn't get consumed evaporate
+        [(step-time factory) move]
+        (:building factory)
+
+
+        )
+
+      ;; incompatible reaction.. flawed model?
+      [a b]))
+
+
+
+
+
+(defn mix-and-react [reaction mols]
+  (let [mixed (partition 2 (shuffle mols))]
+    (mapcat reaction mixed)))
+
+(def molecules (range 2 101))
+
+(defn reaction-cycle [reaction n]
+  (loop
+      [i n mols molecules]
+      (if
+          (zero? i)
+          mols
+          (recur
+           (dec i)
+           (mix-and-react reaction mols)))))
+
+(take 10 (mix-and-react max-reaction-reducing molecules))
+
+(let [reactions (reaction-cycle max-reaction-reducing 10000)]
+  (-> reactions distinct sort))
+
+
+
+
 
 ;; the momemnt I have enough resources to build a robot,
 ;; there is a fork in the road I suppose
 ;; 1. don't build
 ;; 2. for each robot I /can/ build, build
 
+;;  I can say the acc is the stuff I found out so far
+;; then I go
+
+
+;; then I get the edges of where I can make time go forward still
+;; then I split the descicions
+;; add it to the queue of possible work
+;; recur the function
+;; step the time everywhere where I can
+
+
+
+
 ;; can I ever build more than 1? Potentially yes
 ;; there might be a situation of either building 1 or 2 other ones
 ;; so I should find all possible building combinations
 ;; nvm, I can only build 1 at a time
-
-
-
-(defn max-geodes [blueprint time {:keys [robots resources]}]
-  (if
-      (zero? time)
-      0
-      (let [ore (:ore resources)
-            clay (:clay resources)
-            obsidian (:obsidian resources)
-            options [0
-                     (max-geodes blueprint (dec time) (assoc resources :ore (inc ore)))]
-            options (conj options (max-geodes blueprint (dec time) (assoc resources :clay (inc clay))))]
-        (if
-            (contains? blueprint :obsidian-collecting-robot)
-            (let [cost (:obsidian-collecting-robot blueprint)]
-              (conj
-               options
-               (max-geodes
-                blueprint
-                (dec time)
-                (assoc
-                 resources
-                 :ore
-                 (inc (- ore (:ore cost)))
-                 :clay
-                 (inc (- clay (:clay cost)))))))
-            (if
-                (contains?
-                 blueprint
-                 :geode-collecting-robot)
-                (let [cost (:geode-collecting-robot
-                            blueprint)]
-                  (conj
-                   options
-                   (max-geodes
-                    blueprint
-                    (dec time)
-                    (assoc
-                     resources
-                     :ore
-                     (inc (- ore (:ore cost)))
-                     :obsidian
-                     (inc
-                      (- obsidian (:obsidian cost))))))))
-            (apply max options)))))
