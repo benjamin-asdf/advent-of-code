37272f82453d8c8a221f9c783a2b99780da5af5f
Benni Schwerdtner <Benjamin.Schwerdtner@gmail.com>
2024-12-01 20:56:29 +0100

    Day1

---
+++ src/Y2024/day1.clj
@@ -0,0 +1,57 @@
+(ns Y2024.day1
+  (:require
+   [clojure.data.json :as json]))
+
+
+(defn list-distances
+  [input]
+  (let [list-a (map first (partition 2 input))
+        list-b (map second (partition 2 input))]
+    (reduce +
+      (map (comp abs #(- %1 %2))
+        (sort list-a)
+        (sort list-b)))))
+
+(list-distances
+ (read-string (str "[" (slurp "inputs/2024/1/input1") "]")))
+1388114
+
+(defn sum-frequencies
+  [input]
+  (let [list-a (map first (partition 2 input))
+        list-b (map second (partition 2 input))
+        freqs (frequencies list-b)]
+    (transduce (comp (map (juxt identity #(freqs % 0)))
+                     (map (fn [[nr freq-in-list-b]]
+                            (* nr freq-in-list-b))))
+               +
+               list-a)))
+
+
+(sum-frequencies (read-string (str "[" (slurp "inputs/2024/1/input1") "]")))
+23529853
+
+
+
+(comment
+  ;; example:
+
+  (let [input [3 4 4 3 2 5 1 3 3 9 3 3]
+        list-a (map first (partition 2 input))
+        list-b (map second (partition 2 input))]
+    (reduce +
+            (map (comp abs #(- %1 %2))
+                 (sort list-a)
+                 (sort list-b))))
+
+  (let [input [3 4 4 3 2 5 1 3 3 9 3 3]
+        list-a (map first (partition 2 input))
+        list-b (map second (partition 2 input))
+        freqs (frequencies list-b)]
+    (transduce
+     (comp (map (juxt identity #(freqs % 0)))
+           (map (fn [[nr freq-in-list-b]]
+                  (* nr freq-in-list-b))))
+     +
+     list-a))
+  11)
