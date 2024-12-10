5936e5d3f8740d43404f0b084e29b48f3f1ab652
Benni Schwerdtner <Benjamin.Schwerdtner@gmail.com>
2024-12-10 18:12:20 +0100

    Format

--- src/Y2024/day7.clj
+++ src/Y2024/day7.clj
@@ -4,9 +4,9 @@
              py]))
 
 
-;; now hyperon
+;; hyperon interop
+;; ---------------------------------------------------
 (require-python '[hyperon :as hyp :refer [E S V G]])
-
 
 (do
   (def metta (hyp/MeTTa))
@@ -33,19 +33,10 @@
   [o]
   (py.. o get_object -value))
 
+;; ---------------------------------
 
-;; I'll need to figure out a lot of MeTTa for this....
-
-(def example-input
-  "190: 10 19
-3267: 81 40 27
-83: 17 5
-156: 15 6
-7290: 6 8 6 15
-161011: 16 10 13
-192: 17 8 14
-21037: 9 7 18 13
-292: 11 6 16 20")
+;; https://adventofcode.com/2024/day/7
+(def example-input "")
 
 (defn parse-input [s]
   (into
@@ -55,6 +46,95 @@
         (partial map parse-long)
         #(re-seq #"\d+" %1))
        (clojure.string/split-lines s))))
+
+
+;; build an expression like
+;;
+;; (operator a (operator b c))
+(defn equation-possible-metta
+  [equation m-operator]
+  (let [[_ & inputs] equation]
+    (reduce (fn [acc n]
+              (E m-operator acc (hyp/ValueAtom n)))
+            (hyp/ValueAtom (first inputs))
+            (rest inputs))))
+
+;; just book keeping / setup,
+;; in effect metta:
+;; !(keep-working-equation outcome expr)
+;; where expr is (operator a (operator b ...))
+;; for all inputs
+(defn run
+  [equations m-operator]
+  (let [keep-working-equation (metta-parse-single
+                               "keep-working-equation")]
+    (apply +
+           (map metta-value-obj-value
+                (keep first
+                      (map (fn [equation]
+                             (metta-eval-expr
+                              (E keep-working-equation
+                                 (hyp/ValueAtom (first equation))
+                                 (equation-possible-metta
+                                  equation
+                                  m-operator))))
+                           equations))))))
+
+
+(defn part-1
+  [equations]
+  (run equations (metta-parse-single "(operator-1)")))
+
+(defn part-2
+  [equations]
+  (run equations (metta-parse-single "(operator)")))
+
+;; correct, but not fast enough.
+;; So I got inspired by it and made day7_core_logic.clj
+
+(time (part-1 (parse-input example-input)))
+3749
+(time (part-2 (parse-input example-input)))
+11387
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+;; -----------------------------------
+;; second version
+
+
+(defn run-part
+  [part-op input]
+  (reduce +
+          (map metta-value-obj-value
+               (keep ffirst
+                     (map metta-run!
+                          (map (fn [[outcome & inputs]]
+                                 (format "!(%s %s %s)"
+                                         part-op
+                                         outcome
+                                         (reverse (into (list)
+                                                        inputs))))
+                               (parse-input example-input)))))))
+
+
+(comment
+  (run-part "part-1" (parse-input example-input))
+  3749
+  (run-part "part-2" (parse-input example-input))
+  11387)
 
 ;; idea 1:
 ;; metta script should say
@@ -76,65 +156,3 @@
 
 
 ;; --------------------------
-
-(defn equation-possible-metta
-  [equation m-operator]
-  (let [[_ & inputs] equation]
-    (reduce (fn [acc n]
-              (E m-operator acc (hyp/ValueAtom n)))
-            (hyp/ValueAtom (first inputs))
-            (rest inputs))))
-
-(defn run
-  [equations m-operator]
-  (let [keep-working-equation (metta-parse-single
-                                "keep-working-equation")]
-    (apply +
-      (map metta-value-obj-value
-        (keep first
-              (map (fn [equation]
-                     (metta-eval-expr
-                       (E keep-working-equation
-                          (hyp/ValueAtom (first equation))
-                          (equation-possible-metta
-                            equation
-                            m-operator))))
-                   equations))))))
-
-
-(defn part-1
-  [equations]
-  (run equations (metta-parse-single "(operator-1)")))
-
-(defn part-2
-  [equations]
-  (run equations (metta-parse-single "(operator)")))
-
-;; correct, but not fast enough.
-;; So I got inspired by it and made day7_core_logic.clj
-
-(time (part-1 (parse-input example-input)))
-3749
-(time (part-2 (parse-input example-input)))
-11387
-
-(defn run-part
-  [part-op input]
-  (reduce +
-    (map metta-value-obj-value
-      (keep ffirst
-            (map metta-run!
-              (map (fn [[outcome & inputs]]
-                     (format "!(%s %s %s)"
-                             part-op
-                             outcome
-                             (reverse (into (list)
-                                            inputs))))
-                (parse-input example-input)))))))
-
-
-(comment
-  (run-part "part-1" (parse-input example-input))
-  3749
-  (run-part "part-2" (parse-input example-input))
-  11387)
